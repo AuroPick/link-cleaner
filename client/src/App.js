61e0fd2c6926ea9fe09b61f49b559578ad768103
Aykut Saki <aykutsakisocial@gmail.com>
2020-12-30 00:09:23 +0300

    created client structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import LinkCleaner from "./components/LinkCleaner/LinkCleaner";
+import Column from "./components/Column/Column";
+import NewLinks from "./components/NewLinks/NewLinks";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="link-cleaner-container">
+        <LinkCleaner />
+      </div>
+      <div className="column">
+        <Column />
+      </div>
+      <div className="new-links-container">
+        <NewLinks />
+      </div>
     </div>
   );
 }
