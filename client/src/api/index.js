7285002f941768b84a6d5a0842d4164a958414a2
Aykut Saki <aykutsakisocial@gmail.com>
2021-01-21 15:19:46 +0300

    added getLinks request

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 const url = "http://localhost:5000/links";
 
+export const getLinks = (pageNumber) => axios.get(`${url}?page=${pageNumber}`);
 export const getNewLinks = () => axios.get(`${url}/get-links`);
 export const postLink = (link) => axios.post(url, link);
