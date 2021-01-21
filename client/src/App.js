a3e954c74ae5df0e3c2f2e5ba7d6a049230211fb
Aykut Saki <aykutsakisocial@gmail.com>
2021-01-21 15:21:37 +0300

    added react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import { getNewLinks } from "./actions/links";
+import { getNewLinks, getLinks } from "./actions/links";
 import LinkCleaner from "./components/LinkCleaner/LinkCleaner";
 import Column from "./components/Column/Column";
 import NewLinks from "./components/NewLinks/NewLinks";
+import AllLinks from "./components/AllLinks/AllLinks";
 
 import styles from "./App.module.css";
 
@@ -13,20 +15,43 @@ function App() {
 
   useEffect(() => {
     dispatch(getNewLinks());
+    dispatch(getLinks(0));
   }, [dispatch]);
 
   return (
-    <div className={styles.container}>
-      <div className={styles["link-cleaner-container"]}>
-        <LinkCleaner />
-      </div>
-      <div className={styles.column}>
-        <Column />
-      </div>
-      <div className={styles["new-links-container"]}>
-        <NewLinks />
-      </div>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <div className={styles.container}>
+            <div className={styles["link-cleaner-container"]}>
+              <LinkCleaner />
+            </div>
+            <div className={styles.column}>
+              <Column />
+            </div>
+            <div className={styles["new-links-container"]}>
+              <NewLinks />
+            </div>
+          </div>
+        </Route>
+        <Route exact path="/links">
+          <AllLinks />
+        </Route>
+        <Route path="*">
+          <div className={styles.container}>
+            <div className={styles["link-cleaner-container"]}>
+              <LinkCleaner />
+            </div>
+            <div className={styles.column}>
+              <Column />
+            </div>
+            <div className={styles["new-links-container"]}>
+              <NewLinks />
+            </div>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
