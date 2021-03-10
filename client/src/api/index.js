c315a1f51b430d4022ff702197d5e3ac80bf989a
Aykut Saki <aykutsakisocial@gmail.com>
2021-03-10 22:18:13 +0300

    added backend url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:5000/links";
+const url = "https://link-cleaner.herokuapp.com/links";
 
 export const getLinks = (pageNumber) => axios.get(`${url}?page=${pageNumber}`);
 export const getNewLinks = () => axios.get(`${url}/get-links`);
