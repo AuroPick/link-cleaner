23a1c5330742447710dc72333a9d03733097362d
Aykut Saki <aykutsakisocial@gmail.com>
2021-03-10 22:16:52 +0300

    routes moved to own file

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
-import AllLinks from "./components/AllLinks/AllLinks";
+import AllLinks from "./pages/AllLinks/AllLinks";
 import Home from "./pages/Home";
-import Sitemap from "./Sitemap";
 
 const Routes = () => {
   return (
     <Switch>
-      <Route exact path="/" component={Home} sitemapIndex={true} priority="1" />
-      <Route exact path="/links" component={AllLinks} sitemapIndex={true} />
-      <Route exact path="/sitemap" component={Sitemap} sitemapIndex={true} />
-      <Route path="*" component={Home} sitemapIndex={true} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/links" component={AllLinks} />
+      <Route path="*" component={Home} />
     </Switch>
   );
 };
