1242328a582a2370420b27d6f0bda6ccee6d436b
Aykut Saki <aykutsakisocial@gmail.com>
2021-04-07 20:47:35 +0300

    added lottie animation and 404 page

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import AllLinks from "./pages/AllLinks/AllLinks";
+import { Error404 } from "./pages/Error404/Error404";
 import Home from "./pages/Home";
 
 const Routes = () => {
@@ -9,7 +10,7 @@ const Routes = () => {
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/links" component={AllLinks} />
-      <Route path="*" component={Home} />
+      <Route path="*" component={Error404} />
     </Switch>
   );
 };
